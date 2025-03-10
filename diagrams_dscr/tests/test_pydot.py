50b901576d7d91eabc70cda9de91c4d7eb8ab9d5
denirz <denirz@gmail.com>
2025-03-10 07:09:25 +0300

    Pydot Experimants

---
+++ diagrams_dscr/tests/test_pydot.py
@@ -0,0 +1,31 @@
+import pytest
+import pydot
+
+
+from AS17_1prom import diag
+@pytest.fixture
+def diagram_true():
+    return diag
+
+def test_rea(capsys,diagram_true):
+    with capsys.disabled():
+        # print(diagram_true.dot)
+        graphs = pydot.graph_from_dot_data(str(diagram_true.dot))
+        g = graphs[0]
+        print(type(g))
+        for noder in g.get_node_list():
+            print(noder.get_attributes())
+
+        # for i in g.get_subgraph_list():
+        #     print(f"attrs_string = {i.get_name()}")
+        #     print(i.get_node_list())
+        #     for  j in i.get_subgraph_list():
+        #         print(j.get_label())
+        #         print(j.get_node_list())
+        #         for k in j.get_node_list():
+        #             print(k.get_label())
+        # print(graphs[0])
+        # print(graphs[0].get_nodes())
+        # for i,node  in enumerate(graphs[0].get_node_list()):
+        #     print(i)
+        #     print(node.get_name())
