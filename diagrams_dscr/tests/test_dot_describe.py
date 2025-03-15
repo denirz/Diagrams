18eabe822c2f608c7cf731fd9ef0da7c0ad1e183
denirz <denirz@gmail.com>
2025-03-15 16:20:19 +0300

    black Applied

--- diagrams_dscr/tests/test_dot_describe.py
+++ diagrams_dscr/tests/test_dot_describe.py
@@ -4,16 +4,20 @@ from diagrams_dscr.dot_describe import DescribeDiagram
 
 
 from .AS17_1prom import diag
+
+
 @pytest.fixture
 def diagram_true():
     return diag
 
+
 from .test_diagrams import diagram_init
 
-def test_describe_digaram(capsys,diagram_true):
+
+def test_describe_digaram(capsys, diagram_true):
     dd = DescribeDiagram(diagram_true)
     print(dd.filename)
-    assert isinstance(dd,DescribeDiagram)
+    assert isinstance(dd, DescribeDiagram)
     dd.get_subgraphs()
     print(f"{len(dd.nodes)} Nodes detected")
     print(f"{len(dd.edges)} Edges detected")
@@ -22,16 +26,16 @@ def test_describe_digaram(capsys,diagram_true):
     # print(dd.nodes)
     # print(dd.edges)
     for n in dd.nodes:
-        print(n['id'])
-        if n['id'] in ('graph','node','edge'):
+        print(n["id"])
+        if n["id"] in ("graph", "node", "edge"):
             print(n)
             continue
 
 
-def test_describe_digaram_enrichedges(capsys,diagram_true):
+def test_describe_digaram_enrichedges(capsys, diagram_true):
     with capsys.disabled():
         dd = DescribeDiagram(diagram_true)
-        assert isinstance(dd,DescribeDiagram)
+        assert isinstance(dd, DescribeDiagram)
         dd.get_subgraphs()
         print(f"{len(dd.nodes)} Nodes detected")
         print(f"{len(dd.edges)} Edges detected")
@@ -40,26 +44,34 @@ def test_describe_digaram_enrichedges(capsys,diagram_true):
             assert "source_name" in edge.keys()
             assert "dest_name" in edge.keys()
             print(edge)
-            if edge['source_name'] in ('graph','node','edge'):
+            if edge["source_name"] in ("graph", "node", "edge"):
                 print(edge)
                 continue
 
 
-def test_output_edges(capsys,diagram_true):
+def test_output_edges(capsys, diagram_true):
     with capsys.disabled():
         dd = DescribeDiagram(diagram_true)
         dd.outputEdges()
-def  test_outputEdges_fieldlist(capsys,diagram_true):
+
+
+def test_outputEdges_fieldlist(capsys, diagram_true):
     with capsys.disabled():
         dd = DescribeDiagram(diagram_true)
         res = dd.outputEdges(listFields=True)
         print(res)
-        assert isinstance(res,list)
-        dd.outputEdges(Fields=['source_name','dest_name','headlabel','label','description'])
-def  test_outputEdges_fieldlist(capsys,diagram_init):
+        assert isinstance(res, list)
+        dd.outputEdges(
+            Fields=["source_name", "dest_name", "headlabel", "label", "description"]
+        )
+
+
+def test_outputEdges_fieldlist(capsys, diagram_init):
     with capsys.disabled():
         dd = DescribeDiagram(diagram_init)
         res = dd.outputEdges(listFields=True)
         print(res)
-        assert isinstance(res,list)
-        dd.outputEdges(Fields=['source_name','dest_name','headlabel','label','description'])
+        assert isinstance(res, list)
+        dd.outputEdges(
+            Fields=["source_name", "dest_name", "headlabel", "label", "description"]
+        )
